6d6d1eb2d93f94a815138167713de611d2fefc7f
dugrossy baptiste <dugrossy.baptiste@gmail.com>
2019-02-11 10:55:49 +0100

    Creation of a route and a controller where i remember i have to add before commiting

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,8 +8,12 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController
 {
-
+    public function homepage()
+    {
+        return new Response('OMG! My first Symfony page :D');
+    }
 }
